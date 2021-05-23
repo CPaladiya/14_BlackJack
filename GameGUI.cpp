1e8a2fb0c42e0906711ba4fbfa8c26d63ac9050f
cpaladiya <chirag.paladiya@hotmail.com>
2021-05-23 09:06:21 -0400

    Feat : Add looping reduce func

--- GameGUI.cpp
+++ GameGUI.cpp
@@ -10,11 +10,11 @@
 Window::Window(QWidget *parent) : QWidget(parent){
 
     QGridLayout *GameGrid = new QGridLayout;
-    GameGrid->addWidget(DealersBox(),0,0,2,3);
-    GameGrid->addWidget(PlayersBox(),2,0,2,3);
-    GameGrid->addWidget(DealerFundBox(),0,3,1,1);
-    GameGrid->addWidget(PlayerFundBox(),1,3,1,1);
-    GameGrid->addWidget(WhoIsPlayingBox(),2,3,1,1);
+    GameGrid->addWidget(CardBox("Dealer"),0,0,2,3);
+    GameGrid->addWidget(CardBox("Player"),2,0,2,3);
+    GameGrid->addWidget(FundBox("Dealer"),0,3,1,1);
+    GameGrid->addWidget(WhoIsPlayingBox(),1,3,1,1);
+    GameGrid->addWidget(FundBox("Player"),2,3,1,1);
     GameGrid->addWidget(ActionBox(),3,3,1,1);
 
     setLayout(GameGrid);
@@ -22,99 +22,59 @@ Window::Window(QWidget *parent) : QWidget(parent){
     resize(1200,800);
 }
 
-QGroupBox *Window::DealersBox(){
-
-    QGroupBox *DealerGroupBox = new QGroupBox;  //creating a group box
+//Generates main Card tiles where cards will appear for both, dealer and player
+QGroupBox *Window::CardBox(QString participant){
+    
+    QGroupBox *MainBox = new QGroupBox;  //creating a main group box
     QGridLayout *InternalBox = new QGridLayout; //creating a grid to put within the box
-    QPushButton *sampleBut1 = new QPushButton("Dealer Sample1"); //creating first button
-    QPushButton *samplebut2 = new QPushButton("Dealer Sample2"); //creating Second button
+    QPushButton *sampleBut1 = new QPushButton(participant); //creating first button
+    QPushButton *samplebut2 = new QPushButton(participant); //creating Second button
     InternalBox->addWidget(sampleBut1,0,0,1,1); //Adding first button to grid
     InternalBox->addWidget(samplebut2,1,0,5,1); //Adding second button to grid
-    DealerGroupBox->setLayout(InternalBox); //Adding grid to the group box
+    MainBox->setLayout(InternalBox); //Adding grid to the group box
 
-    return DealerGroupBox;
-
-}
-
-
-QGroupBox *Window::PlayersBox(){
-
-    QGroupBox *PlayerGroupBox = new QGroupBox;  //creating a group box
-    QGridLayout *InternalBox = new QGridLayout; //creating a grid to put within the box
-    QPushButton *sampleBut1 = new QPushButton("Player Sample1"); //creating first button
-    QPushButton *samplebut2 = new QPushButton("Player Sample2"); //creating Second button
-    InternalBox->addWidget(sampleBut1,0,0,1,1); //Adding first button to grid
-    InternalBox->addWidget(samplebut2,1,0,5,1); //Adding second button to grid
-    PlayerGroupBox->setLayout(InternalBox); //Adding grid to the group box
-
-    return PlayerGroupBox;
+    return MainBox;
 
 }
 
-QGroupBox *Window::DealerFundBox(){
+QGroupBox *Window::FundBox(QString participant){
 
-    QGroupBox *DealerFundBox = new QGroupBox;  //creating a group box
+    QGroupBox *MainBox = new QGroupBox;  //creating a group box
     QGridLayout *InternalBox = new QGridLayout; //creating a grid to put within the box
-    QPushButton *sampleBut1 = new QPushButton("Player Sample1"); //creating first button
-    QPushButton *samplebut2 = new QPushButton("Player Sample2"); //creating Second button
+    QPushButton *sampleBut1 = new QPushButton(participant); //creating first button
+    QPushButton *samplebut2 = new QPushButton(participant); //creating Second button
     InternalBox->addWidget(sampleBut1,0,0); //Adding first button to grid
     InternalBox->addWidget(samplebut2,1,0); //Adding second button to grid
-    DealerFundBox->setLayout(InternalBox); //Adding grid to the group box
+    MainBox->setLayout(InternalBox); //Adding grid to the group box
 
-    return DealerFundBox;
-
-}
-
-QGroupBox *Window::PlayerFundBox(){
-
-    QGroupBox *PlayerFundBox = new QGroupBox;  //creating a group box
-    QGridLayout *InternalBox = new QGridLayout; //creating a grid to put within the box
-    QPushButton *sampleBut1 = new QPushButton("Player Sample1"); //creating first button
-    QPushButton *samplebut2 = new QPushButton("Player Sample2"); //creating Second button
-    InternalBox->addWidget(sampleBut1,0,0); //Adding first button to grid
-    InternalBox->addWidget(samplebut2,1,0); //Adding second button to grid
-    PlayerFundBox->setLayout(InternalBox); //Adding grid to the group box
-
-    return PlayerFundBox;
+    return MainBox;
 
 }
 
 QGroupBox *Window::WhoIsPlayingBox(){
 
-    QGroupBox *WhoIsBox = new QGroupBox;  //creating a group box
+    QGroupBox *MainBox = new QGroupBox;  //creating a group box
     QGridLayout *InternalBox = new QGridLayout; //creating a grid to put within the box
     QPushButton *sampleBut1 = new QPushButton("Player Sample1"); //creating first button
     QPushButton *samplebut2 = new QPushButton("Player Sample2"); //creating Second button
     InternalBox->addWidget(sampleBut1,0,0); //Adding first button to grid
     InternalBox->addWidget(samplebut2,1,0); //Adding second button to grid
-    WhoIsBox->setLayout(InternalBox); //Adding grid to the group box
+    MainBox->setLayout(InternalBox); //Adding grid to the group box
 
-    return WhoIsBox;
+    return MainBox;
 
 }
-
 
 QGroupBox *Window::ActionBox(){
 
-    QGroupBox *ActionBox = new QGroupBox;  //creating a group box
+    QGroupBox *MainBox = new QGroupBox;  //creating a group box
     QGridLayout *InternalBox = new QGridLayout; //creating a grid to put within the box
     QPushButton *sampleBut1 = new QPushButton("Player Sample1"); //creating first button
     QPushButton *samplebut2 = new QPushButton("Player Sample2"); //creating Second button
     InternalBox->addWidget(sampleBut1,0,0); //Adding first button to grid
     InternalBox->addWidget(samplebut2,1,0); //Adding second button to grid
-    ActionBox->setLayout(InternalBox); //Adding grid to the group box
+    MainBox->setLayout(InternalBox); //Adding grid to the group box
 
-    return ActionBox;
+    return MainBox;
 
 }
-
-
-
-
-
-
-//int main(int argc, char *argv[]){
-
-
-
-//}
--- GameGUI.h
+++ GameGUI.h
@@ -7,21 +7,24 @@
 #include <QtWidgets/QGridLayout>
 #include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QPushButton>
+#include <vector>
+#include <QTextStream>
+
+using namespace std;
 
 class Window : public QWidget{
 
-    Q_OBJECT;
+    //Q_OBJECT;
+    enum Color {Red, Green}; //enum color used in defining color of the tile headings
 
 public:
     Window(QWidget *parent = nullptr);
 
 private:
-    QGroupBox *DealersBox();
-    QGroupBox *PlayersBox();
-    QGroupBox *DealerFundBox();
-    QGroupBox *PlayerFundBox();
-    QGroupBox *WhoIsPlayingBox();
-    QGroupBox *ActionBox();
+    QGroupBox *CardBox(QString participant); //Generates main Card tiles where cards will appear for both, dealer and player
+    QGroupBox *FundBox(QString participant); //Generates smaller tiles on right side where fund will appear
+    QGroupBox *WhoIsPlayingBox(); // Generates smaller tile on right side showing who is playing and the message
+    QGroupBox *ActionBox(); //Generates smaller tile on right side where prompt will be given for user to act on
 };
 
 #endif
