6c6b8d32df0e55536b5d7b30089d56ec1759419c
Chirag Paladiya <chirag.paladiya@hotmail.com>
2021-06-17 23:53:24 -0400

    Chore : Card .h and .cpp commented

--- Card.cpp
+++ Card.cpp
@@ -10,10 +10,11 @@
 
 using namespace std;
 
+//This class will show image in the QLabel, each card will have its own QLabel and its image
 Card::Card(QWidget *parent) : 
     QLabel(parent), CardName_("TT"), CardValue_(0) {
 
-    //Loading card with blank image to set the size of the QLable, as game goes on, the blank card will be gone
+    //Loading card with blank image to set the size of the QLable, as game goes on, the blank card will be replaced with real card image
     QPixmap loadedImage("/home/cpaladiya/My_prog_projects/14_BlackJack/cards/"+CardName_+".png"); //loading the image
     CardImageQLabel_ = new QLabel();//creating a new label
     //Defining variables to resize the image according to the size of the label itself
@@ -25,20 +26,25 @@ Card::Card(QWidget *parent) :
 
 Card::~Card(){}
 
+//Function to reload the true card using given argument of QString
 void Card::ReloadTrueCard (QString NewCardQString){
 
     //clearing the previous content of the blank image label and setting the newCard instead
 
-    CardImageQLabel_->clear();
+    CardImageQLabel_->clear(); //Clearing of the previous blank image, so the size will remain same just the content is cleared
     CardName_ = NewCardQString;
     QPixmap loadedImage("/home/cpaladiya/My_prog_projects/14_BlackJack/cards/"+CardName_+".png"); //loading the image
     //Defining variables to resize the image according to the size of the label itself
-    int LabelHeight = CardImageQLabel_->height();
+    int LabelHeight = CardImageQLabel_->height(); //setting height and width of QLabel
     int LabelWidth = CardImageQLabel_->width();
+    //Setting Pixmap to the QLabel
     CardImageQLabel_->setPixmap(loadedImage.scaled(LabelWidth,LabelHeight, Qt::KeepAspectRatio, Qt::SmoothTransformation));
 
+    //Fade in animation for newly loaded card
     FadeInAnimation();
-    //refreshing the card value as per new card
+    //refreshing the card value as per new card name.
+    //storing the value of card .i.e 11 for Ace, 2 for 2 of Hearts and 10 for any face values
+    //For example, "1J.jpg", CardName_ would be "1J" and its value will be 1
     RefreshCardValue();
 
 }
@@ -75,18 +81,20 @@ void Card::FadeOutAnimation(){
 void Card::RefreshCardValue(){
 
     vector<QString> CardValueNumber {"2","3","4","5","6","7","8","9"};
-
+    //Comparing first letter of the CardName_ to determine the card value
     if (CardName_[0] == "1"){
         CardValue_ = 11; //if ace we will set card value to 11
     }
     else if (CardName_[0] == "T" || CardName_[0] == "0" ){
-        CardValue_ = 0;
-    }
+        CardValue_ = 0; //If dummy blank card or closed card, value will be 0
+    }//Now, we will loop through all the number in CardValueNumber to see if the value CardName_[0] is in it, 
+    //if it is not, it will return 0 which means false. Otherwise it would be a number other than 0
+    //and it will act as True
     else if(count(CardValueNumber.begin(),CardValueNumber.end(), CardName_[0])){
         CardValue_ = CardName_[0].digitValue(); //converting Qstring char to int
     }
     else{
-        CardValue_ = 10;
+        CardValue_ = 10; //For any face value other than Ace, its 10
     }
 }
 
